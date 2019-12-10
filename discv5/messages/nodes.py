c3b556c8db06bc64e026a3386185f9c88e50f595
Dmitrii Shmatko <leodex23@gmail.com>
2019-12-10 20:46:30 +0300

    Fix typing in nodes message

--- discv5/messages/nodes.py
+++ discv5/messages/nodes.py
@@ -1,3 +1,5 @@
+from typing import List
+
 from enr import ENR
 from messages.message import Message
 
@@ -5,9 +7,9 @@ from messages.message import Message
 class NodesMessage(Message):
     nodes: list
 
-    def __init__(self, nodes: list, sender: ENR) -> None:
+    def __init__(self, nodes: List[ENR], sender: ENR) -> None:
         super().__init__(sender)
         self.nodes = nodes
 
-    def get(self) -> list:
+    def get(self) -> List[ENR]:
         return self.nodes
