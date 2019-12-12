741889988964eccdb88ec706a0309b225aec5cc4
Dmitrii Shmatko <leodex23@gmail.com>
2019-12-12 19:01:13 +0300

    Starting declaration of topology in simulation

--- discv5/simulation.py
+++ discv5/simulation.py
@@ -3,7 +3,6 @@ import ns.core
 import ns.network
 import ns.csma
 import ns.internet
-import ns.point_to_point
 import ns.applications
 import sys
 
@@ -22,18 +21,8 @@ if verbose == "True":
 	ns.core.LogComponentEnable("UdpEchoServerApplication", ns.core.LOG_LEVEL_INFO)
 nCsma = 1 if int(nCsma) == 0 else int(nCsma)
 
-p2pNodes = ns.network.NodeContainer()
-p2pNodes.Create(2)
-
 csmaNodes = ns.network.NodeContainer()
-csmaNodes.Add(p2pNodes.Get(1))
 csmaNodes.Create(nCsma)
-
-pointToPoint = ns.point_to_point.PointToPointHelper()
-pointToPoint.SetDeviceAttribute("DataRate", ns.core.StringValue("5Mbps"))
-pointToPoint.SetChannelAttribute("Delay", ns.core.StringValue("2ms"))
-
-p2pDevices = pointToPoint.Install(p2pNodes)
 
 csma = ns.csma.CsmaHelper()
 csma.SetChannelAttribute("DataRate", ns.core.StringValue("100Mbps"))
@@ -42,34 +31,31 @@ csma.SetChannelAttribute("Delay", ns.core.TimeValue(ns.core.NanoSeconds(6560)))
 csmaDevices = csma.Install(csmaNodes)
 
 stack = ns.internet.InternetStackHelper()
-stack.Install(p2pNodes.Get(0))
 stack.Install(csmaNodes)
 
 address = ns.internet.Ipv4AddressHelper()
-address.SetBase(ns.network.Ipv4Address("10.1.1.0"), ns.network.Ipv4Mask("255.255.255.0"))
-p2pInterfaces = address.Assign(p2pDevices)
-
 address.SetBase(ns.network.Ipv4Address("10.1.2.0"), ns.network.Ipv4Mask("255.255.255.0"))
 csmaInterfaces = address.Assign(csmaDevices)
 
-echoServer = ns.applications.UdpEchoServerHelper(9)
-
-serverApps = echoServer.Install(csmaNodes.Get(nCsma))
-serverApps.Start(ns.core.Seconds(1.0))
-serverApps.Stop(ns.core.Seconds(10.0))
-
-echoClient = ns.applications.UdpEchoClientHelper(csmaInterfaces.GetAddress(nCsma), 9)
-echoClient.SetAttribute("MaxPackets", ns.core.UintegerValue(1))
-echoClient.SetAttribute("Interval", ns.core.TimeValue(ns.core.Seconds (1.0)))
-echoClient.SetAttribute("PacketSize", ns.core.UintegerValue(1024))
-
-clientApps = echoClient.Install(p2pNodes.Get(0))
-clientApps.Start(ns.core.Seconds(2.0))
-clientApps.Stop(ns.core.Seconds(10.0))
+# TODO
+#
+# echoServer = ns.applications.UdpEchoServerHelper(9)
+#
+# serverApps = echoServer.Install(csmaNodes.Get(nCsma))
+# serverApps.Start(ns.core.Seconds(1.0))
+# serverApps.Stop(ns.core.Seconds(10.0))
+#
+# echoClient = ns.applications.UdpEchoClientHelper(csmaInterfaces.GetAddress(nCsma), 9)
+# echoClient.SetAttribute("MaxPackets", ns.core.UintegerValue(1))
+# echoClient.SetAttribute("Interval", ns.core.TimeValue(ns.core.Seconds (1.0)))
+# echoClient.SetAttribute("PacketSize", ns.core.UintegerValue(1024))
+#
+# clientApps = echoClient.Install(p2pNodes.Get(0))
+# clientApps.Start(ns.core.Seconds(2.0))
+# clientApps.Stop(ns.core.Seconds(10.0))
 
 ns.internet.Ipv4GlobalRoutingHelper.PopulateRoutingTables()
 
-pointToPoint.EnablePcapAll("second")
 csma.EnablePcap ("second", csmaDevices.Get (1), True)
 
 ns.core.Simulator.Run()
