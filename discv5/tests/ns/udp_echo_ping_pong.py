d50bea836135bdddac68154d2a04e58b8fb768a9
Dmitrii Shmatko <leodex23@gmail.com>
2019-12-12 20:28:28 +0300

    Modified UDP echo server example to omit usage of UDPEchoServerHelper

---
+++ discv5/tests/ns/udp_echo_ping_pong.py
@@ -0,0 +1,52 @@
+# Rewrite of UDP echo client/server example to use custom UDPEchoServer
+
+import ns.applications
+import ns.core
+import ns.internet
+import ns.network
+import ns.point_to_point
+import pdb
+
+ns.core.LogComponentEnable("UdpEchoClientApplication", ns.core.LOG_LEVEL_INFO)
+ns.core.LogComponentEnable("UdpEchoServerApplication", ns.core.LOG_LEVEL_INFO)
+
+nodes = ns.network.NodeContainer()
+nodes.Create(2)
+
+pointToPoint = ns.point_to_point.PointToPointHelper()
+pointToPoint.SetDeviceAttribute("DataRate", ns.core.StringValue("5Mbps"))
+pointToPoint.SetChannelAttribute("Delay", ns.core.StringValue("2ms"))
+
+devices = pointToPoint.Install(nodes)
+
+stack = ns.internet.InternetStackHelper()
+stack.Install(nodes)
+
+address = ns.internet.Ipv4AddressHelper()
+address.SetBase(ns.network.Ipv4Address("10.1.1.0"),
+                ns.network.Ipv4Mask("255.255.255.0"))
+
+interfaces = address.Assign(devices)
+######
+serverNodes = [nodes.Get(1)]
+echoServer = ns.applications.UdpEchoServer()
+echoServer.SetAttribute("Port", ns.core.UintegerValue(13))
+for node in serverNodes:
+    # pdb.set_trace()
+    node.AddApplication(echoServer)
+
+serverApps = ns.network.ApplicationContainer(echoServer)
+serverApps.Start(ns.core.Seconds(1.0))
+serverApps.Stop(ns.core.Seconds(10.0))
+
+echoClient = ns.applications.UdpEchoClientHelper(interfaces.GetAddress(1), 13)
+echoClient.SetAttribute("MaxPackets", ns.core.UintegerValue(1))
+echoClient.SetAttribute("Interval", ns.core.TimeValue(ns.core.Seconds(1.0)))
+echoClient.SetAttribute("PacketSize", ns.core.UintegerValue(1024))
+
+clientApps = echoClient.Install(nodes.Get(0))
+clientApps.Start(ns.core.Seconds(2.0))
+clientApps.Stop(ns.core.Seconds(10.0))
+
+ns.core.Simulator.Run()
+ns.core.Simulator.Destroy()
